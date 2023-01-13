8fd97d51d1a95c7ab857cb90982b6c97fb1f328c
André Campanhã <andre.campanha42@gmail.com>
2023-01-13 14:27:37 +0000

    Fixed station not updating after rename bug

--- generate_samples_json.py
+++ generate_samples_json.py
@@ -5,12 +5,12 @@ from datetime import datetime, timedelta
 base_temperature = random.uniform(15, 22)
 base_wind = random.uniform(23, 40)
 base_humidity = random.uniform(0.1, 0.7)
-# now = datetime.now()
-now = datetime.fromisoformat("2022-12-27T14:00")
+now = datetime.now()
+# now = datetime.fromisoformat("2022-12-27T14:00")
 
 samples = []
 
-for i in range(60):
+for i in range(100):
     timestamp = (now + timedelta(minutes=i)).isoformat()
     wind = random.uniform(base_wind * 0.9, base_wind * 1.1)
     humidity = random.uniform(base_humidity * 0.9, base_humidity * 1.1)
